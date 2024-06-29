f3945272de2417a17b08c5891bbfa327275f6c9f
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-06-29 17:25:54 -0400

    removes extra commands

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -10,26 +10,6 @@ export function activate(context: vscode.ExtensionContext) {
       sidebarProvider
     )
   );
-
-  // Register a custom command
-  context.subscriptions.push(
-    vscode.commands.registerCommand("myextension.askquestion", async () => {
-      let response = await vscode.window.showInformationMessage(
-        "How are you doing?",
-        "Good",
-        "Bad"
-      );
-      if (response === "Bad") {
-        vscode.window.showInformationMessage("I'm sorry");
-      }
-    })
-  );
-
-  context.subscriptions.push(
-    vscode.commands.registerCommand("myextension.sayhello", () => {
-      vscode.window.showInformationMessage("Hello World!");
-    })
-  );
 }
 
 // this method is called when your extension is deactivated
