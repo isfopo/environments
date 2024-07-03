2133dd18a5b86497677d8941a7ae1e74437e2ceb
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-07-03 07:51:49 -0400

    updates name

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ export function activate(context: vscode.ExtensionContext) {
   const sidebarProvider = new SidebarProvider(context.extensionUri);
   context.subscriptions.push(
     vscode.window.registerWebviewViewProvider(
-      "easi-env-sidebar",
+      "environments-sidebar",
       sidebarProvider
     )
   );
