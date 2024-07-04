41cefb7fc0c0d7f1f93de391ac1b927ff4008f6b
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-07-04 06:46:58 -0400

    adds preprocess for typescript

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 import css from "rollup-plugin-css-only";
+import sveltePreprocess from "svelte-preprocess";
 import typescript from "@rollup/plugin-typescript";
 import path from "node:path";
 import fs from "node:fs";
@@ -23,6 +24,7 @@ export default fs
       },
       plugins: [
         svelte({
+          preprocess: sveltePreprocess(),
           compilerOptions: {
             // enable run-time checks when not in production
             dev: !production,
