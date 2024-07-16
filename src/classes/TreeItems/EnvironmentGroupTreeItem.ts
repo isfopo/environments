d157677e8b226816618555ad9e475b5cab2660b3
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-07-16 19:28:34 -0400

    adds default to children

--- src/classes/TreeItems/EnvironmentGroupTreeItem.ts
+++ src/classes/TreeItems/EnvironmentGroupTreeItem.ts
@@ -5,12 +5,13 @@ export class EnvironmentGroupTreeItem extends vscode.TreeItem {
   constructor(
     public readonly name: string,
     public readonly presets: string[],
-    public readonly children: EnvironmentKeyValueTreeItem[],
+    public readonly children: EnvironmentKeyValueTreeItem[] = [],
     public readonly collapsibleState: vscode.TreeItemCollapsibleState = vscode
       .TreeItemCollapsibleState.Collapsed
   ) {
     super(name, collapsibleState);
     this.contextValue = "group";
     this.presets = presets;
+    this.children = children;
   }
 }
